28f9087b6b8ec27cb047918b439d122c9b211afc
Vilas <ekstraplass11@gmail.com>
2024-05-12 03:36:07 +0200

    add nix solution for lsp

--- lua/david/plugins/lsp.lua
+++ lua/david/plugins/lsp.lua
@@ -14,12 +14,22 @@ return {
     },
 
     config = function()
+        --Lsp zero stuff
         local lsp_zero = require('lsp-zero')
 
         lsp_zero.on_attach(function(client, bufnr)
             lsp_zero.default_keymaps({ buffer = bufnr })
         end)
 
+        lsp_zero.set_server_config({
+            on_init = function(client)
+                client.server_capabilities.semanticTokensProvider = nil
+            end,
+        })
+
+
+        --Loading of language servers
+        --Use this on normal distro
         require('mason').setup({})
         require('mason-lspconfig').setup({
             ensure_installed = {
@@ -38,6 +48,26 @@ return {
             },
         })
 
+        --Use this duct tape solution for nix type devflow(add lsp in shell.nix)
+        local servers = {
+            'tsserver',
+            'eslint',
+            'lua_ls',
+            'nixd',
+            'rust_analyzer',
+            'svelte',
+            'pylsp',
+            'clangd',
+            'bashls',
+            'quick_lint_js',
+        }
+        for i,server in pairs(servers)
+        do
+            lsp_zero.default_setup(server)
+        end
+
+
+        --Completion setup
         local cmp = require('cmp')
         local cmp_action = require('lsp-zero').cmp_action()
 
@@ -61,10 +91,11 @@ return {
             },
         })
 
-        local signs = { Error = "x", Warn = "x", Hint = "󰌶 ", Info = " " }
+        local signs = { Error = "x", Warn = "x", Hint = "!", Info = " " }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
             vim.fn.sign_define(hl, { text = '', linehl = '', texthl = hl, numhl = hl })
         end
+
     end
 }
