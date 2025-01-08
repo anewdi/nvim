ccfbb7e28c0ec8c7c42e641a0603929c6d95262e
David <ekstraplass11@gmail.com>
2025-01-08 15:43:54 +0100

    update and add md renderer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,12 @@
 return {
 	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+	{
 		"norcalli/nvim-colorizer.lua",
 	}, --Undotree
 	{
